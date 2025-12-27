1848d9a99dcd8c6e7bbfb878e9d00e500940794b
SelMt82 <>
2025-12-27 13:01:44 +0000

    Dropping Columns

--- the_very_first_steps/dropping_columns/main.py
+++ the_very_first_steps/dropping_columns/main.py
@@ -8,7 +8,16 @@ cars_data = {'model': ['Audi A1', 'Audi A6', 'Audi A4', 'Audi A3','Audi A1'],
 audi_cars = pd.DataFrame(cars_data)
 
 # Write your code below
-audi_cars = audi_cars.drop(columns=['capital'], axis=1) # using columns= for deleting columns followed with ['columnname'], axis=1
+# audi_cars = audi_cars.drop(columns=['capital'], axis=1) # using columns= for deleting columns followed with ['columnname'], axis=1
 # audi_cars = audi_cars.drop(index=audi_cars.index[[1,2]]) use this to delete rows by df index
+# How to drop rows which is including 
+# Example 1 with boolean mask
+# audi_cars = audi_cars[audi_cars['capital'] != 'Monaco']
+# Example 2 with indexvalue of filtered rows
+# first find label-rows, where capital == 'Monaco'
+idx = audi_cars[audi_cars['capital'] == 'Monaco'].index
+# drop those labels
+audi_cars = audi_cars.drop(index=idx)
+
 # Testing the result
 print(audi_cars)
